6596a0b4d7e0f2f5dfd4e9bf5d4aa16fe1ba3a84
zhangchi <zhangchi@microsoft.com>
2026-05-22 15:20:36 +0800

    fix: untrack node_modules and dist (should be git-ignored)

--- server/dist/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./functions/listPhotos");
-require("./functions/uploadPhoto");
-require("./functions/deletePhoto");
-//# sourceMappingURL=index.js.map
